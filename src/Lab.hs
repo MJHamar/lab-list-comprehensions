9a2f03926a51c3645af0c186e1f28532eb4e347d
Miklos Hamar <Miklos.Hamar@warwick.ac.uk>
2020-01-27 19:34:35 +0000

    factorials?

--- src/Lab.hs
+++ src/Lab.hs
@@ -23,14 +23,14 @@ lowercase = ['a'..'z']
 -- 4. Using a list comprehension, define a list of the powers of two for
 -- the exponents from 1 to 10.
 powersOfTwo :: Num a => [a]
-powersOfTwo = [2^n | n <- [0..10]]
+powersOfTwo = [2^n | n <- [1..10]]
 
 -- 5. Using a list comprehension, define a list of the factorials of the
 -- numbers 1 to 10.
 factorials :: [Int]
-factorials = undefined
+factorials = 
     where
-        fac = undefined
+        fac = [x | x <- [1..10]]
 
 -- 6. Using a list comprehension, define a list of coordinates where the top
 -- left corner is (0,0) and the bottom right is (10,10).
@@ -41,20 +41,20 @@ coords = [(x,y) | x <- [0..10], y <- [0..10]]
 -- are in the range from 0 to 5, such that the sum of both sections never
 -- exceeds 5.
 noMoreThanFive :: (Num a, Enum a, Ord a) => [(a,a)]
-noMoreThanFive = [(x,y) | x <- [0..10], x < 5, y <- [0..10], y < 5, (x + y) <= 5]
+noMoreThanFive = [(x,y) | x <- [0..10], x <= 5, y <- [0..10], y <= 5, (x + y) <= 5]
 
 noMoreThanFive' :: (Num a, Enum a) => [(a,a)]
-noMoreThanFive' = [(x,y) | x <- [0..5], y <- [0..5], ]
+noMoreThanFive' = [(x,y) | x <- [0..5], y <- [0..5-x]]
 
 -- 8. Using a list comprehension with a predicate, define a list of all even
 -- numbers from 0 to 100.
 evens :: [Int]
-evens = undefined
+evens = [n | n <- [0..100], mod n 2 == 0]
 
 -- 9. Write a function which, given some natural number n, produces a list of
 -- all multiples of 3 and 5 in the range of 0 to n (inclusive). For example,
 -- multiples 10 = [0,3,5,6,9,10]
 multiples :: Int -> [Int]
-multiples n = undefined
+multiples n = [x | x <- [0..n], (mod x 3 == 0) || (mod x 5 == 0)]
 
 --------------------------------------------------------------------------------
